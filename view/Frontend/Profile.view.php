48fb459db2b65ed67cc92a4f922b7cea59e9db67
MA Web <staal.mikkel@gmail.com>
2018-03-14 16:38:48 +0100

    Profile page - Completed & Responsive

---
+++ view/Frontend/Profile.view.php
@@ -0,0 +1,25 @@
+<div class="profile">
+    <h2>Min side</h2>
+    <img src="./assets/images/profile_placeholder.jpg" alt="">
+    <div class="profile-information">
+
+            <p>Navn:</p>
+            <p>Lorentz Ibson</p>
+
+            <p>Email:</p>
+            <p>something@something.dk</p>
+
+            <p>Færdighedsniveau:</p>
+            <p>Begynder</p>
+
+            <p>Roede kilometre</p>
+            <p>42 km</p>
+    </div>
+    <div class="profile-information">
+        <p>Tilmeldt:</p>
+        <div class="upcoming-events">
+                <p>Kajaktur 15-06-2016</p>
+                <p>Kajaktur 26-06-2016</p>
+        </div>
+    </div>
+</div>
