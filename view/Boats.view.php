ae498734c0addf8a9c424c269c56a813b1a80a46
MA Web <staal.mikkel@gmail.com>
2018-03-14 15:34:31 +0100

    Boats - Responsive

--- view/Boats.view.php
+++ view/Boats.view.php
@@ -7,57 +7,57 @@
                 <th>Sværhedsgrad:</th>
                 <th>Antal:</th>
                 <th class="boats-table-image">image</th>
-                <th>Til salg</th>
+                <th>Til salg:</th>
             </tr>
         </thead>
         <tbody>
             <tr class="boats-category">
                 <td>Havkajakker</td>
             </tr>
-            <tr>
-                <td>Hasle Explorer med finne (K1)</td>
-                <td></td>
-                <td>2 stk</td>
-                <td><img src="./assets/images/kajak01.jpg" alt=""></td>
-                <td>1.500 DKK</td>
+            <tr class="content-row">
+                <td data-column="Type:">Hasle Explorer med finne (K1)</td>
+                <td data-column="Sværhedsgrad:"></td>
+                <td data-column="Antal:">2 stk</td>
+                <td data-column="image" class="boat-image"><img src="./assets/images/kajak01.jpg" alt=""></td>
+                <td data-column="Til Salg:">1.500 DKK</td>
             </tr>
-            <tr>
-                <td>Hasle Explorer med finne (K1)</td>
-                <td></td>
-                <td>2 stk</td>
-                <td><img src="./assets/images/kajak01.jpg" alt=""></td>
-                <td>1.500 DKK</td>
+            <tr class="content-row">
+                <td data-column="Type:">Hasle Explorer med finne (K1)</td>
+                <td data-column="Sværhedsgrad:"></td>
+                <td data-column="Antal:">2 stk</td>
+                <td data-column="image" class="boat-image"><img src="./assets/images/kajak01.jpg" alt=""></td>
+                <td data-column="Til Salg:">1.500 DKK</td>
             </tr>
-            <tr>
-                <td>Hasle Explorer med finne (K1)</td>
-                <td></td>
-                <td>2 stk</td>
-                <td><img src="./assets/images/kajak01.jpg" alt=""></td>
-                <td>1.500 DKK</td>
+            <tr class="content-row">
+                <td data-column="Type:">Hasle Explorer med finne (K1)</td>
+                <td data-column="Sværhedsgrad:"></td>
+                <td data-column="Antal:">2 stk</td>
+                <td data-column="image" class="boat-image"><img src="./assets/images/kajak01.jpg" alt=""></td>
+                <td data-column="Til Salg:">1.500 DKK</td>
             </tr>
             <tr class="boats-category">
                 <td>Havkajakker</td>
             </tr>
-            <tr>
-                <td>Hasle Explorer med finne (K1)</td>
-                <td></td>
-                <td>2 stk</td>
-                <td><img src="./assets/images/kajak01.jpg" alt=""></td>
-                <td>1.500 DKK</td>
+            <tr class="content-row">
+                <td data-column="Type:">Hasle Explorer med finne (K1)</td>
+                <td data-column="Sværhedsgrad:"></td>
+                <td data-column="Antal:">2 stk</td>
+                <td data-column="image" class="boat-image"><img src="./assets/images/kajak01.jpg" alt=""></td>
+                <td data-column="Til Salg:">1.500 DKK</td>
             </tr>
-            <tr>
-                <td>Hasle Explorer med finne (K1)</td>
-                <td></td>
-                <td>2 stk</td>
-                <td><img src="./assets/images/kajak01.jpg" alt=""></td>
-                <td>1.500 DKK</td>
+            <tr class="content-row">
+                <td data-column="Type:">Hasle Explorer med finne (K1)</td>
+                <td data-column="Sværhedsgrad:"></td>
+                <td data-column="Antal:">2 stk</td>
+                <td data-column="image" class="boat-image"><img src="./assets/images/kajak01.jpg" alt=""></td>
+                <td data-column="Til Salg:">1.500 DKK</td>
             </tr>
-            <tr>
-                <td>Hasle Explorer med finne (K1)</td>
-                <td></td>
-                <td>2 stk</td>
-                <td><img src="./assets/images/kajak01.jpg" alt=""></td>
-                <td>1.500 DKK</td>
+            <tr class="content-row">
+                <td data-column="Type:">Hasle Explorer med finne (K1)</td>
+                <td data-column="Sværhedsgrad:"></td>
+                <td data-column="Antal:">2 stk</td>
+                <td data-column="image" class="boat-image"><img src="./assets/images/kajak01.jpg" alt=""></td>
+                <td data-column="Til Salg:">1.500 DKK</td>
             </tr>
         </tbody>
     </table>
